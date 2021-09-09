8b1cee9da17d267346b6fc3a60a77d73b5481498
Artem Karaman <karaman55_09@mail.ru>
2021-09-10 00:51:28 +0300

    add additional info and bind keycode to controll main menu

--- Assets/!Scripts/MainMenuManager.cs
+++ Assets/!Scripts/MainMenuManager.cs
@@ -4,6 +4,21 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     private string _sceneToUnload;
+    [SerializeField]
+    private GameObject _mainMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            _mainMenu.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _mainMenu.SetActive(false);
+        }
+    }
 
     public void LoadScene(string sceneName)
     {
